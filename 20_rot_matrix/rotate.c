900163f8c86a79032e6cfa7086e723d634e4131e
Coursera Learner <nobody@nowhere.nul>
2020-07-22 18:02:35 +0000

    submitting

--- 20_rot_matrix/rotate.c
+++ 20_rot_matrix/rotate.c
@@ -12,8 +12,7 @@ void rotate(char matrix[10][10]) {
   }
   for (int row = 0; row < 10; row++) {
     for (int col = 0; col < 10; col++) {
-      printf("%c", rotatedMatrix[row][col]);
+      matrix[row][col] = rotatedMatrix[row][col];
     }
-    printf("\n");
   }
 }
