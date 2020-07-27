e0ca95428fb8c1bcf49bee28c954ce4d161b5e22
Coursera Learner <nobody@nowhere.nul>
2020-07-27 06:11:36 +0000

    submit

---
+++ 23_power_rec/power.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned power(unsigned x, unsigned y) {
+  if (x == 0 && y == 1) {
+    return 0;
+  }
+  if (y < 0) {
+    return 0;
+  }
+  // base case
+  if (y == 0) {
+    return 1;
+  }
+  // x ^ y = (x ^ y - 1) * x, recursively called
+  return power(x, y - 1) * x;
+}
