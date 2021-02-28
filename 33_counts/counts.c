16f0dff25b389dbeeedd6774a4b71ff68a8e0b82
Coursera Learner <nobody@nowhere.nul>
2021-02-28 23:07:07 +0000

    adding files

--- 33_counts/counts.c
+++ 33_counts/counts.c
@@ -2,16 +2,76 @@
 #include <stdlib.h>
 #include <string.h>
 #include "counts.h"
-counts_t * createCounts(void) {
-  //WRITE ME
-}
-void addCount(counts_t * c, const char * name) {
-  //WRITE ME
-}
-void printCounts(counts_t * c, FILE * outFile) {
-  //WRITE ME
+#define MAX_BUFFER_SIZE 10
+#define NOT_FOUND 0
+
+counts_t* createCounts(void) {
+  counts_t* counts = (counts_t*)malloc(sizeof(counts_t));
+
+  counts->size = MAX_BUFFER_SIZE;
+  counts->itemCount = 0;
+  counts->countArray = (one_count_t*)malloc(sizeof(one_count_t) * MAX_BUFFER_SIZE);
+  counts->unknownCount = 0;
+
+  return counts;
 }
 
-void freeCounts(counts_t * c) {
-  //WRITE ME
+void addCount(counts_t* c, const char* name) {
+  if (name == NULL) {
+    c->unknownCount++;
+    return;
+  }
+
+  for (int i = 0; i < c->itemCount; i++) {
+    // loops through the countArray, checks if names are equal
+    //if equal you found the item in countArray
+    //if not equal, keep looping
+    if (strcmp(c->countArray[i].name, name) == 0) {
+      c->countArray[i].count++;
+      return;
+    }
+
+  }
+
+  if (c->itemCount == c->size) {
+    //increment size FIRST, must do this or will realloc as same size and overrun buffer
+    c->size += MAX_BUFFER_SIZE;
+    c->countArray = (one_count_t*)realloc(c->countArray, c->size * sizeof(one_count_t));
+
+  }
+
+  // if we haven't returned already, the name is a new item
+  c->countArray[c->itemCount].count = 1;
+  //allocate space for name string
+  //allocate +1 for '\0' so that strcpy will have space to write it into the name field
+  c->countArray[c->itemCount].name = (const char*)malloc(sizeof(const char) * strlen(name) + 1);
+  //write string to the slot
+  c->countArray[c->itemCount].name = (const char*)strcpy((char*)c->countArray[c->itemCount].name, name);
+
+  //only increment after writing newItem to countArray
+  //makes sure index is correct
+  c->itemCount++;
+}
+
+void printCounts(counts_t* c, FILE* outFile) {
+
+  for (int i = 0; i < c->itemCount; i++) {
+    fprintf(outFile, "%s: %d\n", c->countArray[i].name, c->countArray[i].count);
+  }
+
+  if (c->unknownCount > 0) {
+    fprintf(outFile, "<unkown> : %d\n", c->unknownCount);
+  }
+}
+
+void freeCounts(counts_t* c) {
+  //free name in each slot of c->countArray
+  for (int i = 0; i < c->itemCount; i++) {
+    free((void*)c->countArray[i].name);
+  }
+  //free slots in c->countArray
+  free(c->countArray);
+  //free c
+  free(c);
+
 }
--- 33_counts/counts_test.c
+++ 33_counts/counts_test.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "counts.h"
 
-#define NUM_TESTS 12
+#define NUM_TESTS 16
 int main(void) {
-  char * testData[NUM_TESTS] = {"apple", "banana", NULL,"apple",
-				"frog","sword","bear",NULL,
-				"frog","apple", "zebra", "knight"};
-  counts_t * testCounts= createCounts();
-  for(int i =0; i < NUM_TESTS; i++) {
-    addCount(testCounts,testData[i]);
+  const char* testData[NUM_TESTS] = { "apple", "banana", NULL,"apple",
+				      "frog","sword","bear", NULL,
+				      "frog","apple", "zebra", "knight", "pear",
+				      "duck", "chicken", "cow"};
+  counts_t* testCounts = createCounts();
+  for (int i = 0; i < NUM_TESTS; i++) {
+    addCount(testCounts, testData[i]);
   }
+  assert(testCounts->unknownCount == 2);
+  assert(testCounts->itemCount == 11);
+  assert(testCounts->countArray[5].count == 1);
+  assert(strcmp(testCounts->countArray[10].name, "cow") == 0);
+  assert(testCounts->countArray[10].count == 1);
+
   printCounts(testCounts, stdout);
   freeCounts(testCounts);
   return EXIT_SUCCESS;
